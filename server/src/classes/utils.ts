2a6ffc47e2ff81a3027c94289fb1a596586370a1
MswTester <mswtester10417@gmail.com>
2023-12-28 22:42:17 +0900

    Change:added-eventEmitter-listener-remove

--- server/src/classes/utils.ts
+++ server/src/classes/utils.ts
@@ -204,16 +204,42 @@ export function isIntersecting(polygon: Polygon, polygon2: Polygon): boolean {
     return false;
 }
 
-export class EventEmitter{
-    private events:Map<string, Function[]> = new Map();
-    on(event:string, callback:Function){
-        if(!this.events.has(event)) this.events.set(event, []);
-        this.events.get(event)?.push(callback);
+export class EventEmitter {
+    private events: Map<string, Function[]> = new Map();
+
+    on(event: string, callback: Function) {
+        let listeners = this.events.get(event);
+        if (!listeners) {
+            listeners = [];
+            this.events.set(event, listeners);
+        }
+        listeners.push(callback);
     }
-    emit(event:string, ...args:any[]){
-        if(!this.events.has(event)) return;
-        this.events.get(event)?.forEach(callback => {
-            callback(...args);
-        })
+
+    emit(event: string, ...args: any[]) {
+        const listeners = this.events.get(event);
+        if (!listeners) return;
+        for (const listener of listeners) {
+            listener(...args);
+        }
+    }
+
+    // 이벤트 리스너 제거 기능
+    off(event: string, callback: Function) {
+        const listeners = this.events.get(event);
+        if (!listeners) return;
+        const index = listeners.indexOf(callback);
+        if (index !== -1) {
+            listeners.splice(index, 1);
+        }
+    }
+
+    // 모든 이벤트 리스너 제거
+    removeAllListeners(event?: string) {
+        if (event) {
+            this.events.delete(event);
+        } else {
+            this.events.clear();
+        }
     }
 }
